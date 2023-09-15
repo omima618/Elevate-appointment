4521ea2d64832e506976b3bf7b136f50eeaf94de
Omima Khaled <85596970+omima618@users.noreply.github.com>
2023-09-16 01:53:15 +0300

    update form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// LANGUAGE DROPDOWN
 document.querySelectorAll('.toggler-btn').forEach((btn) => {
     btn.addEventListener('click', function () {
         document.getElementById(`${this.dataset.toggle}`).classList.toggle('hidden');
@@ -7,4 +8,28 @@ document.querySelectorAll('.toggler-btn').forEach((btn) => {
 document.addEventListener('click', function (e) {
     if (e.target.classList.contains('.dropdown') || e.target.closest('.dropdown')) return;
     document.getElementById('language-dropdown').classList.add('hidden');
+});
+
+// SWITCH SERVICES
+
+const switchService = (currentService) => {
+    document.querySelectorAll('[data-service]').forEach((service) => {
+        if (service.dataset.service === currentService) {
+            setTimeout(() => { service.classList.remove('hidden'); }, 202);
+            setTimeout(() => { service.classList.remove('opacity-0'); }, 400);
+            service.querySelector('select').id = 'service';
+        } else {
+            service.classList.add('opacity-0');
+            setTimeout(() => { service.classList.add('hidden'); }, 200);
+            service.querySelector('select').id = '';
+        }
+    });
+}
+
+document.querySelectorAll('input[name="service-type"]').forEach((inp) => {
+    inp.addEventListener('change', function () { 
+        if (this.checked) {
+            switchService(this.value);
+        }
+    })
 })
